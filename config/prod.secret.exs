f045d3b2e3c9ebeeab43e454d580238f5e6459fd
Abey Onalaja <abey.onalaja@gmail.com>
2017-07-04 20:55:56 +0100

    Add prod.secret

---
+++ config/prod.secret.exs
@@ -0,0 +1,12 @@
+use Mix.Config
+
+# In this file, we keep production configuration that
+# you likely want to automate and keep it away from
+# your version control system.
+#
+# You should document the content of this
+# file or create a script for recreating it, since it's
+# kept out of version control and might be hard to recover
+# or recreate for your teammates (or you later on).
+config :example_phx, ExamplePhx.Web.Endpoint,
+  secret_key_base: "f4qSy0nXE8yM2vakdGjeHeSOX+iqfeTWv9Zu5Xik6ogLjAmf4S2N+Tw0s81pqd5u"
